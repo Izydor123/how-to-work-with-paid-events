2c5ea9e623ffb6130f0d61c0d365608b0c97f3a7
Izydor123 <djqyubi@gmail.com>
2025-04-04 13:35:49 +0100

    change route

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'import.meta.env.VITE_API_URL',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },

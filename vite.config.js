399a07b7b724be318ce60dddd1ce0a0687beaa10
Izydor123 <djqyubi@gmail.com>
2025-04-07 10:39:15 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: process.env.PORT_URL, 
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   base: "/how-to-work-with-paid-events"
 });

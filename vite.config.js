ce02c159317dd69d6e519e65e59623ae5e6170dd
Izydor123 <djqyubi@gmail.com>
2025-04-07 10:55:34 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-dotenv.config();
-
 export default defineConfig({
   plugins: [react()],
   base: "/how-to-work-with-paid-events"
